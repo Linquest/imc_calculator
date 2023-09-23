24d3c5b422b4312d8153c9068599a915119f9ee7
Linquest <lfap19@gmail.com>
2023-09-23 11:31:36 -0300

    feat: finalizado primeiro desafio criando projeto de IMC com Flutter

--- bin/imc_calculator.dart
+++ bin/imc_calculator.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
-import 'src/pessoa.dart';
-import 'src/calculadora_imc.dart';
+import '../lib/src/pessoa.dart';
+import '../lib/src/calculadora_imc.dart';
 
 void main() {
   print('Digite o seu nome:');
-  String nome = stdin.readLineSync("Luis");
+  String nome = stdin.readLineSync()!;
 
   print('Digite o seu peso em kg:');
-  double peso = double.parse(stdin.readLineSync());
+  double peso = double.parse(stdin.readLineSync()!);
 
   print('Digite a sua altura em metros:');
-  double altura = double.parse(stdin.readLineSync());
+  double altura = double.parse(stdin.readLineSync()!);
 
   Pessoa pessoa = Pessoa(nome, peso, altura);
   CalculadoraIMC calculadora = CalculadoraIMC(pessoa);
